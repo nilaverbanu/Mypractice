eb87af65d22f286c6cda55496b181c90e6023714
nilaverbanu <nilaverbanu2@gmail.com>
2016-01-11 15:02:17 +0530

    Revert "Revert "Revert "Revert "Revert "del sys comment"""""
    This reverts commit 50fd5ccf85e7cd41b408b9cd042c1a01f98b3413.

--- Samplegitprojects/src/sample/Firstcode.java
+++ Samplegitprojects/src/sample/Firstcode.java
@@ -6,6 +6,8 @@ public class Firstcode {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
 		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
 	}
 
 }
