e81a999f8e4599ed52e082df6c84eacd6d1b3071
nilaverbanu <nilaverbanu2@gmail.com>
2016-01-11 14:06:30 +0530

    sp1

---
+++ Samplegitprojects/src/sample/Firstcode.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Firstcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+	}
+
+}
